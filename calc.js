e3e7ab8ac954196359124657d472ff92a1963302
Christian Koch <christiank1030@gmail.com>
2022-09-13 16:55:56 -0400

    Third number added

--- calc.js
+++ calc.js
@@ -13,30 +13,32 @@ reader.question("What would you like to calculate?", function(input){
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
+	const num3 = Number(tokens[3])
 	
 	console.log('mathSymbol', mathSymbol);
 	console.log('num1', num1);
 	console.log('num2', num2); 
+	console.log('num3', num3);
 
 	//Addition
 	if(mathSymbol === '+') {
-		console.log(num1 + num2)
+		console.log(num1 + num2 + num3)
 	};
 
 	//Subtraction
 	if(mathSymbol === '-') {
-		console.log(num1 - num2)
+		console.log(num1 - num2 - num3)
 	};
 
 	//Multiplication
 	if(mathSymbol === '*') {
-		console.log(num1 * num2)
+		console.log(num1 * num2 * num3)
 	};
 
 	//Division
 	if(mathSymbol === '/') {
-		console.log(num1 / num2)
-		console.log(num1 % num2)
+		console.log(num1 / num2 / num3)
+		console.log(num1 % num2 % num3)
 	};
 
 	//Square Root
